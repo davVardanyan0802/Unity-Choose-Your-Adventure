c1a8a82880951ec5df7f41e68f182a46bd0f6d49
david <davss0802@gmail.com>
2020-05-17 19:04:32 +0400

    Experimenting with Textmesh Pro

--- Choose Your Adventure Project/Assets/Scripts/AdventureGame.cs
+++ Choose Your Adventure Project/Assets/Scripts/AdventureGame.cs
@@ -6,15 +6,38 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State state;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Helllllloooo";
+        state = startingState;
+        textComponent.text =state.GetStoryText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    void ManageState(){
+        State[] nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            state = nextStates[0];
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            state = nextStates[1];
+        }
+
+         else if(Input.GetKeyDown(KeyCode.Alpha3)){
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStoryText();
     }
 }
--- Choose Your Adventure Project/Assets/Scripts/State.cs
+++ Choose Your Adventure Project/Assets/Scripts/State.cs
@@ -6,4 +6,13 @@ using UnityEngine;
 public class State :ScriptableObject
 {
   [TextArea(10,14)] [SerializeField] string storyText;
+  [SerializeField] State[] nextStates;
+
+  public string GetStoryText(){
+      return storyText;
+  }
+
+  public State[] GetNextStates(){
+      return nextStates;
+  }
 }
