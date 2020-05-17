3e3f86bcafc6eb585962af87e3c2cce935f5df3a
david <davss0802@gmail.com>
2020-05-17 16:53:28 +0400

    Creating scriptable object

---
+++ Choose Your Adventure Project/Assets/Scripts/State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State :ScriptableObject
+{
+  [TextArea(10,14)] [SerializeField] string storyText;
+}
